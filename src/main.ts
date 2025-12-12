3fcbc6321c57b312999853f0c0d233f124099f02
Amanda-m-z <amanda.z.mickaela@gmail.com>
2025-12-12 13:54:23 +0100

    added map and div for info

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,30 @@ import { getStops } from "./services/slService";
 import "./style.css";
 
 const mapFrame = document.getElementById("map") as HTMLIFrameElement;
+const showTheStop = document.getElementById("showResult");
+
+
 
 export const showMap = (lat: number, lon: number): void => {
   if (!mapFrame) return;
   mapFrame.src = `https://maps.google.com/maps?q=${lat},${lon}&z=15&output=embed`;
 };
 
-navigator.geolocation.getCurrentPosition((pos) => {
+navigator.geolocation.getCurrentPosition((pos) => {   //Hämta nuvarande position
   const { latitude, longitude } = pos.coords;
   showMap(latitude, longitude);
 });
 
+//Knapp för användare att skriva in position
 document.getElementById("searchForm")?.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const input = document.getElementById("searchText") as HTMLInputElement | null;
   const searchText = input?.value.trim() || "";
+  if (searchText.length > 0){
+    if (showTheStop) showTheStop.className = "Show"; 
+  }
+  
 
   const stops = await getStops(searchText);
   createHtml(stops);
